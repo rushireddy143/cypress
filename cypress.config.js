9c76e32891ff74f2a45e2b380a34b2be7916d04d
Phashanthacharya1818 <Prashanthacharya0to9@gmail.com>
2023-09-21 17:14:26 +0530

    modified tables script

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,7 @@
 module.exports = {
 	projectId: "nbohd2",
+	screenshotOnRunFailure:false,
+	trashAssetsBeforeRuns:true,
+	video: true,
 	// ...rest of the Cypress project config
 }
--- cypress/integration/examples/Tables.spec.js
+++ cypress/integration/examples/Tables.spec.js
@@ -1,5 +1,6 @@
 describe('table handling', () => {
      
+    
     beforeEach('performance before each it block', () => {
         cy.visit("https://datatables.net/examples")
         cy.get('.active>ul>:nth-child(1)>a').click()
@@ -8,9 +9,11 @@ describe('table handling', () => {
     })
 
     it('check number of rows and colomns', () => {
-
+        
         cy.get("#DataTables_Table_0>tbody>tr").should('have.length','9');
         cy.get("#DataTables_Table_0>thead>tr>th").should('have.length','5');
+        //cy.screenshot()
+        
     })
     
     it('check data in a specific row and colomn', () => {
@@ -25,6 +28,28 @@ describe('table handling', () => {
         .each( ($row, index, $rows) => {
             cy.wrap($row).within( ()=>{
                 cy.get("td").each( ($col, index, $cols)=>{
+                    cy.log($col.text());
+                })
+            })
+        })
+    })
+    
+    it('get a colomn data in table', () => {
+        cy.get("#DataTables_Table_0>tbody>tr")
+        .each( ($row,index,$rows)=>{
+        cy.wrap($row).within( ()=>{
+            cy.get("td:nth-child(2)").each(($col,index,$cols)=>{
+            cy.log($col.text());
+                })
+            })
+        }) 
+    })
+
+    it('get a row data in a table', ()=>{
+        cy.get("#DataTables_Table_0>tbody>tr:nth-child(5)")
+        .each(($row,index,$rows)=>{
+            cy.wrap($row).within(()=>{
+                cy.get("td").each(($col,index,$cols)=>{
                     cy.log($col.text());
                 })
             })
