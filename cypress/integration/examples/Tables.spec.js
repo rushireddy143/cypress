f48139ad63cbe76c0dec1d8d0824325290de7f91
Phashanthacharya1818 <Prashanthacharya0to9@gmail.com>
2023-09-25 19:24:58 +0530

    added pagination function

--- cypress/integration/examples/Tables.spec.js
+++ cypress/integration/examples/Tables.spec.js
@@ -4,12 +4,13 @@ describe('table handling', () => {
     beforeEach('performance before each it block', () => {
         cy.visit("https://datatables.net/examples")
         cy.get('.active>ul>:nth-child(1)>a').click()
-        cy.get('.toc>:nth-child(5)>a').click()
+        //cy.get('.toc>:nth-child(5)>a').click()
+        //cy.get('.toc > :nth-child(11) > a').click()
         
     })
 
     it('check number of rows and colomns', () => {
-        
+        cy.get('.toc>:nth-child(5)>a').click()
         cy.get("#DataTables_Table_0>tbody>tr").should('have.length','9');
         cy.get("#DataTables_Table_0>thead>tr>th").should('have.length','5');
         //cy.screenshot()
@@ -17,13 +18,13 @@ describe('table handling', () => {
     })
     
     it('check data in a specific row and colomn', () => {
-        
+        cy.get('.toc>:nth-child(5)>a').click()
         cy.get("#DataTables_Table_0>tbody>tr:nth-child(8)>td:nth-child(5)")
         .contains('$103,600');
     })
 
     it('get entire data from a table', () => {
-        
+        cy.get('.toc>:nth-child(5)>a').click()
         cy.get("#DataTables_Table_0>tbody>tr")
         .each( ($row, index, $rows) => {
             cy.wrap($row).within( ()=>{
@@ -35,6 +36,7 @@ describe('table handling', () => {
     })
     
     it('get a colomn data in table', () => {
+        cy.get('.toc>:nth-child(5)>a').click()
         cy.get("#DataTables_Table_0>tbody>tr")
         .each( ($row,index,$rows)=>{
         cy.wrap($row).within( ()=>{
@@ -46,6 +48,7 @@ describe('table handling', () => {
     })
 
     it('get a row data in a table', ()=>{
+        cy.get('.toc>:nth-child(5)>a').click()
         cy.get("#DataTables_Table_0>tbody>tr:nth-child(5)")
         .each(($row,index,$rows)=>{
             cy.wrap($row).within(()=>{
@@ -55,4 +58,19 @@ describe('table handling', () => {
             })
         })
     })
+
+    it('Pagination',()=>{
+        cy.get('.toc > :nth-child(11) > a').click()
+            let totalpages;
+            //find total number of pages
+            cy.get('.dataTables_info').then( (e)=>{
+             let mytext=e.text();  //Showing 1 to 10 of 57 entries
+             totalpages=mytext.substring(mytext.indexOf("(")+1,mytext.indexOf("pages")-1);
+             cy.log("Total number of pages in a table===>"+totalpages);  
+            })
+
+
+            
+        })
+
 })
